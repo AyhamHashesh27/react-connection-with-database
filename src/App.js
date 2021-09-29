c421f951168ce168e010d775573e44b328e382c3
ayhamhashesh <ayham.hashesh@gmail.com>
2021-09-29 11:20:25 +0300

    Initialize the code and sending HTTP request

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import MoviesList from "./components/MoviesList";
+import { useState } from "react";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  function fetchMoviesHandler() {
+    // Response required time to be fetched, so we need then
+    fetch("https://swapi.dev/api/films/")
+      .then((response) => {
+        // convert response format
+        return response.json();
+      })
+      .then((data) => {
+        const transformedMovies = data.results.map((movieData) => {
+          // Extract required fileds
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+        return setMovies(transformedMovies);
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </>
   );
 }
 
