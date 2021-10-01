df4a0e376c5dead1504f2d7c09134210552d756f
ayhamhashesh <ayham.hashesh@gmail.com>
2021-10-01 11:17:05 +0300

    End of section 'Section 14: Sending HTTP Requests (e.g. Connecting to a Database)'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import MoviesList from "./components/MoviesList";
 import { useState, useEffect, useCallback } from "react";
+import AddMovie from "./components/AddMovie";
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -14,22 +15,38 @@ function App() {
     try {
       setIsLoaing(true);
       // Response required time to be fetched, so we need then
-      const response = await fetch("https://swapi.dev/api/films/");
+      // const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch("https://react-http-5743c-default-rtdb.firebaseio.com/movies.json");
       if (!response.ok) {
         throw new Error("Something went wrong!");
       }
       // convert response format
       const data = await response.json();
-      const transformedMovies = data.results.map((movieData) => {
-        // Extract required fileds
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      // To Read the data from the Firebase
+      const loadedMovies = [];
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+      setMovies(loadedMovies);
+
+      // **********************************
+      // Transform/get Data from swapi
+      // const transformedMovies = data.results.map((movieData) => {
+      //   // Extract required fileds
+      //   return {
+      //     id: movieData.episode_id,
+      //     title: movieData.title,
+      //     openingText: movieData.opening_crawl,
+      //     releaseDate: movieData.release_date,
+      //   };
+      // });
+      // setMovies(transformedMovies);
+      // **********************************
     } catch (error) {
       setError(error.message);
     }
@@ -38,7 +55,24 @@ function App() {
 
   useEffect(() => {
     fetchMoviesHandler();
-  }, []);
+  }, [fetchMoviesHandler]);
+
+  async function addMovieHandler(movie) {
+    console.log(movie);
+    const response = await fetch(
+      "https://react-http-5743c-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+    // You should add validation try/catch
+  }
 
   let content = <p>Found no movies.</p>;
   if (error) {
@@ -53,6 +87,9 @@ function App() {
 
   return (
     <>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
